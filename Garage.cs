f7aade6a4167dd0cf0ae751ae370d2d494dbb8cf
Olof <olof@true.se>
2020-03-31 12:48:02 +0200

    UnitTest1

---
+++ Garage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Garage_1
+{
+    public class Garage<T>: IEnumerable<T> where T: Vehicle
+    {
+        //private readonly int capacity;
+
+        public Garage(int capacity)
+        {
+            //this.capacity = capacity;
+            Capacity = capacity;
+        }
+
+        public int Capacity { get; }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Vehicle.cs
@@ -0,0 +1,6 @@
+﻿namespace Garage_1
+{
+    public class Vehicle
+    {
+    }
+}
